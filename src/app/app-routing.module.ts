550dccf1b47286f47cb877f2748c429940f80043
Mark Phillips <markphillips@Marks-MacBook-Pro.local>
2018-05-14 21:32:38 -0700

    updates to routing, removed images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { ContactComponent } from './contact/contact.component';
-import { NavComponent } from './nav/nav.component';
 import { GrantComponent } from './grant/grant.component';
 import { MarkComponent } from './mark/mark.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { IndexComponent } from './index/index.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/', pathMatch: 'full' },
-    { path: 'contact', component: ContactComponent },
-    { path: 'nav', component: NavComponent },
     { path: 'grant', component: GrantComponent },
     { path: 'mark', component: MarkComponent },
-    { path: 'projects', component: ProjectsComponent },
     { path: '', component: IndexComponent }
-
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+    imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {
